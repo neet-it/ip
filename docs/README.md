ab4a74fa4e5882dbe9a37d6187089fdc8a0d0443
neet-it <neeti.26deshpande@gmail.com>
2025-08-27 15:44:46 +0800

    Rename Duke

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Cortex User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Cortex.java
@@ -0,0 +1,12 @@
+public class Cortex {
+    public static void main(String[] args) {
+
+        System.out.println("__________________________________________________");
+        System.out.println("Hello! I'm Cortex");
+        System.out.println("What can I do for you?");
+
+        System.out.println("__________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________________________");
+    }
+}
