97020d034bc817b03f784392fb3aedb15e280fe8
neet-it <neeti.26deshpande@gmail.com>
2025-08-27 16:32:28 +0800

    Add lists

--- src/main/java/Cortex.java
+++ src/main/java/Cortex.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cortex {
@@ -12,12 +13,27 @@ public class Cortex {
         System.out.println("\t" +line);
 
         String command = sc.nextLine().trim();
+        ArrayList<String> list = new ArrayList<>();
 
         while(!command.equalsIgnoreCase("bye")) {
-            System.out.println("\t" +line);
-            System.out.println("\t" +command);
-            System.out.println("\t" +line);
+            System.out.println("\t" + line);
 
+            if(command.equalsIgnoreCase("LIST")) {
+                if(list.isEmpty()) {
+                    System.out.println("\t" + "List Empty");
+                } else {
+                    int c = 1;
+                    for(String s : list) {
+                        System.out.println("\t" + c++ + ". " + s);
+                    }
+                }
+
+            } else {
+                list.add(command);
+                System.out.println("\t" + "added: " + command);
+            }
+
+            System.out.println("\t" + line);
             command = sc.nextLine().trim();
         }
 
