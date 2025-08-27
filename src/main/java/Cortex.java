1f99832759ae745949f1f86e21412511f8107300
neet-it <neeti.26deshpande@gmail.com>
2025-08-27 23:02:43 +0800

    Handle Errors

--- src/main/java/Cortex.java
+++ src/main/java/Cortex.java
@@ -29,55 +29,102 @@ public class Cortex {
                     }
                 }
 
-            } else if (command.startsWith("mark ")) {
-                int i = Integer.parseInt(command.substring(5).trim()) - 1;
-                Task t = list.get(i);
+            } else if (command.startsWith("mark")) {
 
-                t.markAsDone();
+                try {
+                   int  i= Integer.parseInt(command.substring(5).trim()) - 1;
+                    if(i < 0 || i >= list.size()) {
+                        System.out.println("\t" + "Invalid Task! Cannot mark task.");
+                    } else {
+                        Task t = list.get(i);
 
-                System.out.println("\t" + "Nice! I've marked this task as done:");
-                System.out.println("\t" + t);
+                        t.markAsDone();
 
-
-            } else if (command.startsWith("unmark ")) {
-                int i = Integer.parseInt(command.substring(7).trim()) - 1;
-                Task t = list.get(i);
-
-                t.unmarkAsDone();
-
-                System.out.println("\t" + "OK, I've marked this task as not done yet:");
-                System.out.println("\t" + t);
-
-            } else {
-                Task task = new Task(command);
-                if (command.startsWith("todo ")) {
-                    String s = command.substring(5).trim();
-                    task = new Todo(s);
-
-                } else if (command.startsWith("deadline ")) {
-                    String s = command.substring(9).trim();
-                    String[] bySplit = s.split("/by");
-                    String desc = bySplit[0].trim();
-                    String by = bySplit[1].trim();
-
-                    task = new Deadline(desc, by);
-
-                } else if (command.startsWith("event ")) {
-                    String s = command.substring(6).trim();
-                    String[] fromSplit = s.split("/from");
-                    String desc = fromSplit[0].trim();
-
-                    String[] toSplit = fromSplit[1].split("/to");
-                    String from = toSplit[0];
-                    String to = toSplit[1];
-
-                    task = new Event(desc, from, to);
+                        System.out.println("\t" + "Nice! I've marked this task as done:");
+                        System.out.println("\t" + t);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("\t" + "Invalid Task! Cannot mark task.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("\t" + "Invalid Task! Cannot mark task.");
                 }
 
-                list.add(task);
-                System.out.println("\t" + "Got it. I've added this task:");
-                System.out.println("\t" + task);
-                System.out.println("\t" + "Now you have " + list.size() + " tasks in the list.");
+            } else if (command.startsWith("unmark")) {
+                try {
+                    int i = Integer.parseInt(command.substring(7).trim()) - 1;
+
+                    if(i < 0 || i >= list.size()) {
+                        System.out.println("\t" + "Invalid Task! Cannot unmark task.");
+                    } else {
+                        Task t = list.get(i);
+
+                        t.unmarkAsDone();
+
+                        System.out.println("\t" + "OK, I've marked this task as not done yet:");
+                        System.out.println("\t" + t);
+                    }
+
+                } catch (NumberFormatException e) {
+                    System.out.println("\t" + "Invalid Task! Cannot unmark task.");
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println("\t" + "Invalid Task! Cannot unmark task.");
+                }
+
+
+            }else {
+                Task task = new Task(command);
+                boolean isValidTask = false;
+                int c = 0;
+                if (command.startsWith("todo")) {
+                    c++;
+                    try {
+                        String s = command.substring(5).trim();
+                        task = new Todo(s);
+                        isValidTask = true;
+                    } catch (Exception e){
+                        System.out.println("\t" + "INVALID TODO TASK");
+                    }
+
+                } else if (command.startsWith("deadline")) {
+                    c++;
+                    try {
+                        String s = command.substring(9).trim();
+                        String[] bySplit = s.split("/by");
+                        String desc = bySplit[0].trim();
+                        String by = bySplit[1].trim();
+
+                        task = new Deadline(desc, by);
+                        isValidTask = true;
+                    } catch (Exception e) {
+                        System.out.println("\t" + "INVALID DEADLINE TASK");
+                    }
+
+                } else if (command.startsWith("event")) {
+                    c++;
+                    try {
+                        String s = command.substring(6).trim();
+                        String[] fromSplit = s.split("/from");
+                        String desc = fromSplit[0].trim();
+
+                        String[] toSplit = fromSplit[1].split("/to");
+                        String from = toSplit[0];
+                        String to = toSplit[1];
+
+                        task = new Event(desc, from, to);
+                        isValidTask = true;
+                    } catch (Exception e) {
+                        System.out.println("\t" + "INVALID EVENT TASK");
+                    }
+                }
+
+                if(isValidTask) {
+                    list.add(task);
+                    System.out.println("\t" + "Got it. I've added this task:");
+                    System.out.println("\t" + task);
+                    System.out.println("\t" + "Now you have " + list.size() + " tasks in the list.");
+                } else if (c == 0){
+                    System.out.println("\t" + "INVALID TASK TYPE");
+                }
 
             }
 
