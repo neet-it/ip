6ef242ce3fec9e7e5fd07adbdc2fbc70c3c535b4
neet-it <neeti.26deshpande@gmail.com>
2025-08-27 17:08:39 +0800

    Mark as Done feature, Add Task class

--- src/main/java/Cortex.java
+++ src/main/java/Cortex.java
@@ -13,23 +13,44 @@ public class Cortex {
         System.out.println("\t" +line);
 
         String command = sc.nextLine().trim();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         while(!command.equalsIgnoreCase("bye")) {
             System.out.println("\t" + line);
 
             if(command.equalsIgnoreCase("LIST")) {
                 if(list.isEmpty()) {
-                    System.out.println("\t" + "List Empty");
+                    System.out.println("\t" + "There are no tasks in your list.");
                 } else {
+                    System.out.println("\t" + "Here are the tasks in your list:");
                     int c = 1;
-                    for(String s : list) {
-                        System.out.println("\t" + c++ + ". " + s);
+                    for(Task t : list) {
+                        System.out.println("\t" + c++ + ". " + "[" + t.getStatusIcon() + "] " + t.getDescription());
                     }
                 }
 
+            } else if (command.startsWith("mark ")) {
+                int i = Integer.parseInt(command.substring(5).trim()) - 1;
+                Task t = list.get(i);
+
+                t.markAsDone();
+
+                System.out.println("\t" + "Nice! I've marked this task as done:");
+                System.out.println("\t" + "[" + t.getStatusIcon() + "] " + t.getDescription());
+
+
+            } else if (command.startsWith("unmark ")) {
+                int i = Integer.parseInt(command.substring(7).trim()) - 1;
+                Task t = list.get(i);
+
+                t.unmarkAsDone();
+
+                System.out.println("\t" + "OK, I've marked this task as not done yet:");
+                System.out.println("\t" + "[" + t.getStatusIcon() + "] " + t.getDescription());
+
             } else {
-                list.add(command);
+                Task task = new Task(command);
+                list.add(task);
                 System.out.println("\t" + "added: " + command);
             }
 
