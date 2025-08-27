da1c1607bf12db98de6599e959fa44b8bd73699e
neet-it <neeti.26deshpande@gmail.com>
2025-08-27 16:14:36 +0800

    Echo Command

--- src/main/java/Cortex.java
+++ src/main/java/Cortex.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Cortex {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("__________________________________________________");
-        System.out.println("Hello! I'm Cortex");
-        System.out.println("What can I do for you?");
+        String line = "__________________________________________________";
 
-        System.out.println("__________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("__________________________________________________");
+        System.out.println("\t" + line);
+        System.out.println("\t" +"Hello! I'm Cortex");
+        System.out.println("\t" +"What can I do for you?");
+        System.out.println("\t" +line);
+
+        String command = sc.nextLine().trim();
+
+        while(!command.equalsIgnoreCase("bye")) {
+            System.out.println("\t" +line);
+            System.out.println("\t" +command);
+            System.out.println("\t" +line);
+
+            command = sc.nextLine().trim();
+        }
+
+        System.out.println("\t" +line);
+        System.out.println("\t" +"Bye. Hope to see you again soon!");
+        System.out.println("\t" +line);
     }
 }
